8e2fac8b25f8f0d523c2eebc3297cac596fb8419
namickey <teethdai@gmail.com>
2022-04-10 07:16:35 +0900

    Changes

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        String word = itemService.getItem(name);
+        return String.format("Hello %s!", name + " " + word);
+    }
+
+    @GetMapping("/bye")
+    public String bye(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Bye %s!", name);
+    }
+}
