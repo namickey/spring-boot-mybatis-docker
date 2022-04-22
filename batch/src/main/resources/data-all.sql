0de66dc0f833bce37bcc7aef2826c86e0e7bbfae
namickey <teethdai@gmail.com>
2022-04-22 11:47:32 +0900

    fix

---
+++ batch/src/main/resources/data-all.sql
@@ -0,0 +1,2 @@
+insert into public.item values('AAA', 'BBB');
+insert into public.item values('CCC', 'DDD');
---
+++ batch/src/main/resources/schema-all.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS ITEM CASCADE;
+
+CREATE TABLE item (
+	id varchar NOT NULL,
+	name varchar NULL,
+	CONSTRAINT item_pk PRIMARY KEY (id)
+);
