6552734a647189f154cacb7a7f0b631394760a2b
namickey <teethdai@gmail.com>
2022-06-05 22:12:32 +0900

    add

---
+++ web-css/src/main/java/com/example/webcss/WebCssApplication.java
@@ -0,0 +1,13 @@
+package com.example.webcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCssApplication.class, args);
+	}
+
+}
