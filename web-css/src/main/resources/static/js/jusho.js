09f3f41e81ecdbf5b066ec227c1688563fe5d2c2
namickey <teethdai@gmail.com>
2022-06-21 00:10:15 +0900

    fix

--- web-css/src/main/resources/static/js/jusho.js
+++ web-css/src/main/resources/static/js/jusho.js
@@ -1,57 +1,73 @@
 function jusho(btnName){
     console.log(btnName);
-    $(document).ready(function(){
-        //TODO モーダル処理の部品化。
-        const optionsModal = {
-            onCloseEnd: function(){
-                //モーダルクローズ時に、モーダル内住所リストをクリア。
-                $('#jusholist').empty();
-            }
-        }
-        const modalElm = document.querySelector('.modal');
-        const instanceModal = M.Modal.init(modalElm, optionsModal);
+}
 
-        //住所検索ボタンイベント設定。クリック後にAjax呼び出し。
-        $('#modal-trigger').on('click', function(){
-            $.ajax({
-                url:'/getJusho',
-                type:'GET',
-                data: {
-                    postCode: $('#postCode').val(),
-                },
-                dataType: 'json',
-                timeout: 3000,
-                xhrFields: {
-                    withCredentials: true
-                },
-            }).done(function(data){
-                //住所1件毎に、イベント設定とラジオボタン要素生成。
-    	        data.forEach(function(item){
-    	            const postCode = item['postCode'];
-    	            const jushoKanji = item['jushoKanji'];
-    	            const jushoKana = item['jushoKana'];
-    	            const radioInput = $('<p><label><input name="ju" type="radio"/><span>'+postCode+' '+jushoKanji+'</span></label></p>');
-    	            //イベント設定。住所が選択されたら、選択された住所を画面本体へ反映する。
-    	            $(radioInput).find('input').on('change', function(){
-    	                $('#postCode').val(postCode);
-    	                $('#jushoKanji').val(jushoKanji);
-    	                $('#jushoKana').val(jushoKana);
-    	            });
-    	            $('#jusholist').append(radioInput);
-    		    });
-    		    //イベント設定。住所が選択されたら、モーダルをクローズする。※radioの選択が見えるように、遅延してクローズ。
-    		    $('#jusholist input').each(function(index, element){
-    		        $(element).on('change', function(){
-    		            setTimeout(() => {instanceModal.close()}, 200);
-    		        });
-    		    });
-    		    //モーダル画面を開く
-    		    instanceModal.open();
-            }).fail(function(data){
-                console.log(data['statusText']);
-                console.log(data['status']);
-                console.log(data['responseText']);
+function getJusho(instanceModal, postCodeVal, postCodeId, jushoKanjiId, jushoKanaId) {
+    $.ajax({
+        url:'/getJusho',
+        type:'GET',
+        data: {
+            postCode: postCodeVal,
+        },
+        dataType: 'json',
+        timeout: 3000,
+        xhrFields: {
+            withCredentials: true
+        },
+    }).done(function(data){
+        $('#jusholist').empty();
+        //住所1件毎に、イベント設定とラジオボタン要素生成。
+        data.forEach(function(item){
+            const postCode = item['postCode'];
+            const jushoKanji = item['jushoKanji'];
+            const jushoKana = item['jushoKana'];
+            const radioInput = $('<p><label><input name="ju" type="radio"/><span>'+postCode+' '+jushoKanji+'</span></label></p>');
+            //イベント設定。住所が選択されたら、選択された住所を画面本体へ反映する。
+            $(radioInput).find('input').on('change', function(){
+                $(postCodeId).val(postCode);
+                $(jushoKanjiId).val(jushoKanji);
+                $(jushoKanaId).val(jushoKana);
+            });
+            $('#jusholist').append(radioInput);
+        });
+        //イベント設定。住所が選択されたら、モーダルをクローズする。※radioの選択が見えるように、遅延してクローズ。
+        $('#jusholist input').each(function(index, element){
+            $(element).on('change', function(){
+                setTimeout(() => {instanceModal.close()}, 200);
             });
         });
+    }).fail(function(data){
+        // TODO エラー時の対応
+        console.log(data['statusText']);
+        console.log(data['status']);
+        console.log(data['responseText']);
+    });
+}
+
+function initJushoModal(triggerBtn, postCodeId, jushoKanjiId, jushoKanaId) {
+    const optionsModal = {
+        onCloseEnd: function(){
+            //モーダルクローズ時に、モーダル内住所リストをクリア。
+            $('#jusholist').empty();
+        }
+    }
+    const modalElm = document.querySelector('.modal');
+    const instanceModal = M.Modal.init(modalElm, optionsModal);
+
+    //モーダル起動ボタン。住所検索ボタンイベント設定。クリック後にAjax呼び出し。
+    $(triggerBtn).on('click', function(){
+        const postCodeVal = $(postCodeId).val();
+        if (postCodeVal == '') {
+            instanceModal.open();
+            return;
+        }
+        $('#postCode-modal').val(postCodeVal)
+        getJusho(instanceModal, postCodeVal, postCodeId, jushoKanjiId, jushoKanaId);
+        instanceModal.open();
+    });
+
+    //モーダル内の検索ボタン
+    $('#modal-search').on('click', function(){
+        getJusho(instanceModal, $('#postCode-modal').val(), postCodeId, jushoKanjiId, jushoKanaId);
     });
 }
---
+++ web-css/src/main/resources/static/js/water.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    initJushoModal('#modal-trigger', '#postCode', '#jushoKanji', '#jushoKana');
+});
